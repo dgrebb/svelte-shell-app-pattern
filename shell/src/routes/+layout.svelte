637fa5bc2cb374dc3ff5b0d9411da5faa52883a6
Dan Grebb <445891+dgrebb@users.noreply.github.com>
2025-12-23 19:58:45 -0500

    feat: add global styles and dark mode support
    - created default.css for UI package with custom properties for light and dark themes
    - updated app.html to apply dark mode class to the body
    - introduced app.css to import global styles and UI defaults
    - refactored layout.svelte to use app.css and removed obsolete layout.css

--- shell/src/routes/+layout.svelte
+++ shell/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import './layout.css';
+	import '$lib/app.css';
 	import favicon from '$lib/assets/favicon.svg';
   import { setContext } from 'svelte';
   import { page } from '$app/state';
